fe1e2662e089168365711e265175c5fa4d49e304
ShearOfDoom <ebr4d@live.com>
2016-06-29 19:27:00 -0500

    Initial commit.

---
+++ CLIGL/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CLIGL")]
+[assembly: AssemblyDescription("A command-line interface graphics library.")]
+[assembly: AssemblyConfiguration("Release")]
+[assembly: AssemblyCompany("Ethan Bierlein")]
+[assembly: AssemblyProduct("CLIGL")]
+[assembly: AssemblyCopyright("Copyright © 2016")]
+[assembly: AssemblyTrademark("CLIGL")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("ba149fed-7c88-47d9-b855-bff06adfbe52")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
